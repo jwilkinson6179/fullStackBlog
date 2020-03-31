6a4f4deb7ac17542343ae4629d0ee59717b1a768
Adam Bennett <nyoxidetwitter@gmail.com>
2020-03-31 15:29:52 -0400

    entities setup properly

--- src/main/java/com/zipcode/fullstackblog/models/Tag.java
+++ src/main/java/com/zipcode/fullstackblog/models/Tag.java
@@ -1,4 +1,54 @@
 package com.zipcode.fullstackblog.models;
 
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
 public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private String name;
+
+    public Tag() {}
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Tag)) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(getId(), tag.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
