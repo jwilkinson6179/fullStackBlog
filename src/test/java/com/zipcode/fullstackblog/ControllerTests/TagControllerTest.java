184ad08cb7c6b4a03ee098f29baa7011a255f43d
ruppal <raheeluppal323@gmail.com>
2020-04-01 16:55:58 -0400

    working on test

--- src/test/java/com/zipcode/fullstackblog/ControllerTests/TagControllerTest.java
+++ src/test/java/com/zipcode/fullstackblog/ControllerTests/TagControllerTest.java
@@ -1,11 +1,14 @@
 package com.zipcode.fullstackblog.ControllerTests;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zipcode.fullstackblog.controllers.TagController;
+import com.zipcode.fullstackblog.models.Post;
 import com.zipcode.fullstackblog.models.Tag;
 import com.zipcode.fullstackblog.services.TagService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,21 +35,29 @@ public class TagControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @InjectMocks
+    private TagController tagController;
+
     @Test
     @DisplayName("POST /tags - Success")
     void testCreateTag() throws Exception {
-        Tag postTag = new Tag(1L,"Hello");
-        Tag mockTag = new Tag("Hello");
+        Tag postTag = new Tag("Hello");
+        Tag mockTag = new Tag(1L,"Hello");
+        Post mockPost = new Post();
+        tagController.save(postTag);
+        mockTag.setPost(mockPost);
         doReturn(mockTag).when(service).create(any());
 
-        mockMvc.perform(post("/car")
+        mockMvc.perform(post("/tags")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(asJsonString(postTag)))
 
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
 
-                .andExpect(header().string(HttpHeaders.LOCATION, "/car/1"))
+                .andExpect(header().string(HttpHeaders.LOCATION, "/tags/1"))
+
+                .andExpect(jsonPath("$.id", is(1)))
                 .andExpect(jsonPath("$.name", is("Hello")));
 
 
