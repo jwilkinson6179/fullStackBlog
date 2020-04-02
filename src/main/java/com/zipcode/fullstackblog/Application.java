d1ab7c81cc9affed0aef31aa024f3d08a9def5b6
James Wilkinson <60611745+jwilkinson6179@users.noreply.github.com>
2020-04-02 19:29:21 -0400

    Remove unnecessary imports
    Import bloat fixed.

--- src/main/java/com/zipcode/fullstackblog/Application.java
+++ src/main/java/com/zipcode/fullstackblog/Application.java
@@ -1,11 +1,7 @@
 package com.zipcode.fullstackblog;
 
-import com.zipcode.fullstackblog.controllers.*;
-import com.zipcode.fullstackblog.models.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 @SpringBootApplication
 public class Application
