4ede8427b11aca20cf948a1da6732a8f341e1765
Greg Davis <jrobley3@hotmail.com>
2020-04-04 22:45:27 -0400

    edit kinda works, seems to be adding instead

--- BlogFrontEnd/LoopyBlog/src/app/posts/post-edit/post-edit.component.ts
+++ BlogFrontEnd/LoopyBlog/src/app/posts/post-edit/post-edit.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GiphyService } from '../../services/giphy.service';
+import { NgForm } from '@angular/forms';
+import { PostService } from '../../services/post/post.service';
 
 @Component({
   selector: 'app-post-edit',
@@ -7,8 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostEditComponent implements OnInit {
 
-  constructor() { }
+  post: any = {};
 
-  ngOnInit(): void {}
+  sub: Subscription;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private postService: PostService,
+              private giphyService: GiphyService) { }
+
+  ngOnInit() {this.sub = this.route.params.subscribe(params => {
+    const id = params['id'];
+    if (id) {
+      this.postService.get(id).subscribe((car: any) => {
+        if (car) {
+          this.post = car;
+          this.post.href = car._links.self.href;
+          // this.giphyService.get(car.name).subscribe(url => car.giphyUrl = url);
+        } else {
+          console.log(`Post with id '${id}' not found, returning to list`);
+          this.gotoList();
+        }
+      });
+    }
+  });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  gotoList() {
+    this.router.navigate(['/post-list']);
+  }
+
+  save(form: NgForm) {
+    this.postService.save(form).subscribe(result => {
+      this.gotoList();
+    }, error => console.error(error));
+  }
+
+  remove(href) {
+    this.postService.remove(href).subscribe(result => {
+      this.gotoList();
+    }, error => console.error(error));
+  }
 
 }
