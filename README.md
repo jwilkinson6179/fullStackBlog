050446086de68b578570a01df97e2b1f3e76d7db
jwilkinson6179 <JamesW6179@gmail.com>
2020-04-13 17:25:11 -0400

    Removed all TypeScript

--- README.md
+++
@@ -1,4 +0,0 @@
-# fullStackBlog
-Full Stack Blogging application by Zipcode Wilmington Cohort 6.0 members
-
-Add more later to this.
--- src/main/java/com/zipcode/fullstackblog/repositories/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zipcode.fullstackblog.repositories;
-
-import com.zipcode.fullstackblog.models.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepository extends JpaRepository<Board, Long>{
-}
